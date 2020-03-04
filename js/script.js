8fd38742107bafdb0620948620b67ee06beec2a6
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-03-04 16:48:19 -0500

    script for counters added

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counter');
+const speed = 80000;
+
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const increment = target / speed;
+
+        if(count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 40);
+        }else{
+            count.innerText = target;
+            counter.innerText = count + "%"
+        }
+    }
+    updateCount();
+});
